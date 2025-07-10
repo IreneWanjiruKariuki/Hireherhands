bb8750e2e19b2e85a59f5baf734a02cdcd48a8b0
irenekariuki <144525530+irenekariuki@users.noreply.github.com>
2025-07-10 16:37:08 +0300

    changes

--- backend/config.py
+++ backend/config.py
@@ -2,9 +2,10 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv(
         'DATABASE_URL',
-        'postgresql://hireuser:securepassword@localhost/hireherhands'
+        'postgresql://hireuser:securepassword@172.20.233.224/hireherhands'
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
     SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')
     JWT_SECRET_KEY = 'supersecretkey'
     ALLOWED_ADMIN_EMAILS = os.getenv('ALLOWED_ADMIN_EMAILS', '').split(',')
+
